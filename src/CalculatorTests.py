b85fa0505afa2fdb44c7fbbfe660ee80c1b7d8fa
elionoula <elionoula@gmail.com>
2021-03-14 14:18:41 -0400

    Fix: Added Missing Multiplication Test

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -25,6 +25,13 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
 
+    def test_multiplication(self):
+        test_data = CsvReader("data/Unit Test Multiplication.csv").data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.result, result)
+
     def test_square(self):
         test_data = CsvReader("data/Unit Test Square.csv").data
         for row in test_data:
