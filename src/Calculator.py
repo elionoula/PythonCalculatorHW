9da88917886bf2cd75263ff71cc448365eb4c69b
elionoula <elionoula@gmail.com>
2021-03-14 13:02:54 -0400

    Feature: Added CSV Reader and Test

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,5 +1,9 @@
 class Calculator:
     result = 0
 
+    def add(self, a, b):
+        self.result = addition(a,b)
+        return addition(a,b)
+
     def __init__(self):
         pass
---
+++ src/CsvReaderTest.py
@@ -0,0 +1,9 @@
+import unittest
+from CsvReader import CsvReader, ClassFactory
+from pprint import pprint
+
+class MyTestCase(unittest.TestCase):
+
+
+    if __name__ =='_main_':
+        unittest.main()
